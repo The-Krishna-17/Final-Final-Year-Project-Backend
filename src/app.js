d92247b8150eabf71e0587e090aaa9dfb7e281ee
Krishna <thekrishnadhami17@gmail.com>
2026-06-08 15:20:41 +0545

    minor change

--- src/app.js
+++ src/app.js
@@ -36,8 +36,8 @@ app.use(
 );
 
 // ─── Body Parsers ─────────────────────────────────────────────────────────────
-app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
 // ─── Global Rate Limiter ──────────────────────────────────────────────────────
